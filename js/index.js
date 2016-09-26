75067038739cda5e42a10a47ad1c53dcaebb5062
byalice <1533674340@qq.com>
2016-09-26 19:03:50 +0800

    finish

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ $(function(){
 		scrollingSpeed: 400,
 		css3: true,
 		resize: true,
+		animateAnchor: false,
 		anchors: ["page1","page2","page3","page4","page5","page6","page7","page8","page9","page10","page11"],
 		menu:'#menu',
 		verticalCentered: true,
